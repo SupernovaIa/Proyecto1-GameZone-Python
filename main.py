57f719352d51426fcb5e3fd11e9efa62d0a062cc
jcarreir <jav.racec@gmail.com>
2024-10-05 19:52:45 +0200

    Game ships. Ships setup done

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from src.support import jugar
+
+jugar()
+
+
+
+"""
 def main():
     print("Welcome")
 
 if __name__ == "__main__":
     main()
+
+"""
---
+++ src/support.py
@@ -0,0 +1,2 @@
+def jugar():
+    print("Jugar")
